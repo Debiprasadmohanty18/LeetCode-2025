b932eb1b9fe46827faa9ab4276c9b02ae4f21e7a
Debiprasad Mohanty <145470177+Debiprasadmohanty18@users.noreply.github.com>
2025-01-08 10:22:56 +0530

    Add files via upload

---
+++ CountPrefixAndSuffixPairsI.java
@@ -0,0 +1,27 @@
+package com.tanmay.LeetCodeEasyProblems;
+
+public class CountPrefixAndSuffixPairsI {
+
+	public static void main(String[] args) {
+		
+		String[] words = {"abab","ab"};
+		System.out.println(countPrefixSuffixPairs(words));
+
+	}
+	
+	static int countPrefixSuffixPairs(String[] words) 
+	{
+        int count = 0;
+        for(int i=0;i<words.length;i++)
+        {
+        	for(int j=i+1;j<words.length;j++)
+        	{
+        		if(words[j].contains(words[i]))
+        			count++;
+        	}
+        }
+        
+        return count;
+    }
+
+}
