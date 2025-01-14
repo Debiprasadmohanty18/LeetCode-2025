4654eaa3d3a01ad63f0474fb0734764fe44ea59c
Debiprasad Mohanty <145470177+Debiprasadmohanty18@users.noreply.github.com>
2025-01-14 12:41:28 +0530

    Update 14.FindThePrefixCommonArrayOfTwoArrays.java

--- January/14.FindThePrefixCommonArrayOfTwoArrays.java
+++ January/14.FindThePrefixCommonArrayOfTwoArrays.java
@@ -6,15 +6,14 @@ Return the prefix common array of A and B.
 A sequence of n integers is called a permutation if it contains all integers from 1 to n exactly once.
 
 Example 1:
-
 Input: A = [1,3,2,4], B = [3,1,2,4]
 Output: [0,2,3,4]
 Explanation: At i = 0: no number is common, so C[0] = 0.
 At i = 1: 1 and 3 are common in A and B, so C[1] = 2.
 At i = 2: 1, 2, and 3 are common in A and B, so C[2] = 3.
 At i = 3: 1, 2, 3, and 4 are common in A and B, so C[3] = 4.
-Example 2:
 
+Example 2:
 Input: A = [2,3,1], B = [3,1,2]
 Output: [0,1,3]
 Explanation: At i = 0: no number is common, so C[0] = 0.
